5c676d1503ca91c9f3918172021136b84c101f71
MichaelaVakadinova <mihaelavakadinova@gmail.com>
2016-11-23 16:29:31 +0200

    Exercize one: Odd or even number

---
+++ ExcerciseOne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.Write("The number is even.");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("The number is odd.");
+                Console.Read();
+            }
+        }
+    }
+}
